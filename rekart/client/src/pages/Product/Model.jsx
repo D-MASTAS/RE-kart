7b64e9924b0671c76ce67b795a8ed4b4901fae30
m99Tanishq <21bcs164@ietdavv.edu.in>
2023-03-26 13:42:03 +0530

    Final Fix

--- rekart/client/src/pages/Product/Model.jsx
+++ rekart/client/src/pages/Product/Model.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Model = () => {
 
-    const [showModal, setShowModal] = useState(true);
+    // const [showModal, setShowModal] = useState(true);
 
   const handleCloseModal = () => {
     
