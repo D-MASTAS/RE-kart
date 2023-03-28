9b12bb3f5ce6a014056d95c2de3e1f9320b57603
m99Tanishq <97497412+m99Tanishq@users.noreply.github.com>
2023-03-28 08:53:14 +0530

    Update Product.jsx

--- rekart/client/src/pages/Product/Product.jsx
+++ rekart/client/src/pages/Product/Product.jsx
@@ -40,7 +40,7 @@ const Product = () => {
     setProvider(provider);
 
     const rekart = new ethers.Contract(
-      "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      "",
       ABI,
       provider
     );
